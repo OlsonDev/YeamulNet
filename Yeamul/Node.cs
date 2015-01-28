c52be29d603d1f94d2c414179f329aa4b4477d07
Olson.dev <Olson.dev@Gmail.com>
2015-01-28 01:13:20 -0600

    Add decimalValue, progress on Node.ToString()

--- Yeamul/Node.cs
+++ Yeamul/Node.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Runtime.InteropServices;
 
 namespace Yeamul {
 	[StructLayout(LayoutKind.Explicit)]
-	public class Node {
+	public class Node { // TODO: Consider converting to struct
 		[FieldOffset(0)] private NodeType type;
 		[FieldOffset(1)] private ScalarType scalarType;
 
@@ -13,14 +14,15 @@ namespace Yeamul {
 
 		[FieldOffset(3)] private bool booleanValue;
 		[FieldOffset(3)] private long longValue;
+		[FieldOffset(3)] private ulong ulongValue;
 		[FieldOffset(3)] private Guid guidValue;
 		[FieldOffset(3)] private double doubleValue;
-		[FieldOffset(3)] private string stringValue; // TODO: Consider always having this
+		[FieldOffset(3)] private decimal decimalValue;
+		[FieldOffset(3)] private string stringValue; // TODO: Consider always having this (own field offset)
 		[FieldOffset(3)] private List<Node> sequenceValue;
 		[FieldOffset(3)] private Dictionary<Node, Node> mapValueNodeKey;
 		[FieldOffset(3)] private Dictionary<string, Node> mapValueStringKey;
-
-		// ReSharper disable once ConvertToAutoProperty
+		
 		public NodeType Type {
 			get { return type; }
 			private set { type = value; }
@@ -58,7 +60,45 @@ namespace Yeamul {
 		}
 
 		private string ToStringAsScalar() {
-			throw new NotImplementedException();
+			switch (scalarType) {
+				case ScalarType.NonScalar:
+					throw new InvalidOperationException();
+				case ScalarType.Null:
+					return "~";
+				case ScalarType.Boolean:
+					return booleanValue.ToString();
+				case ScalarType.Number:
+					return ToStringAsNumber();
+				case ScalarType.String:
+					return stringValue;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+
+		private string ToStringAsNumber() {
+			switch (numberType) {
+				case NumberType.Byte:	// TODO: InvalidOperationException?
+				case NumberType.Char: // TODO: InvalidOperationException?
+				case NumberType.Int16:
+				case NumberType.Int32:
+				case NumberType.Int64:
+					return longValue.ToString();
+				case NumberType.UInt16:
+				case NumberType.UInt32:
+				case NumberType.UInt64:
+					return ulongValue.ToString();
+				case NumberType.Guid:
+					return guidValue.ToString();
+				case NumberType.Float:
+				case NumberType.Double:
+				case NumberType.Extended:
+					return doubleValue.ToString(CultureInfo.InvariantCulture);
+				case NumberType.Decimal:
+					return decimalValue.ToString(CultureInfo.InvariantCulture);
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
 		}
 
 		private string ToStringAsSequence() {
