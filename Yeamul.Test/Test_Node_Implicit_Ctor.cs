de8b3684fa484bc0e778e7178e44ad8162ec98ab
Josh Olson <Olson.dev@Gmail.com>
2015-02-04 14:47:29 -0600

    Add unit tests for implicit integral construction
    Also configured ReSharper to allow Test_UpperCase_Underscore_Tolerant
    classes and namespaces for this solution.

---
+++ Yeamul.Test/Test_Node_Implicit_Ctor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Yeamul {
+	[TestFixture]
+	public class Test_Node_Implicit_Ctor {
+		[Test]
+		public void CanConstructImplicitlyFromInt16() {
+			Node node = Int16.MaxValue;
+			AssertNodeIsDefinedScalarNumber(node);
+			AssertNodeHasTypes(node, NodeType.Scalar, ScalarType.Number, NumberType.Int16, MapType.NotApplicable);
+		}
+
+		[Test]
+		public void CanConstructImplicitlyFromInt32() {
+			Node node = Int32.MaxValue;
+			AssertNodeIsDefinedScalarNumber(node);
+			AssertNodeHasTypes(node, NodeType.Scalar, ScalarType.Number, NumberType.Int32, MapType.NotApplicable);
+		}
+
+		[Test]
+		public void CanConstructImplicitlyFromInt64() {
+			Node node = Int64.MaxValue;
+			AssertNodeIsDefinedScalarNumber(node);
+			AssertNodeHasTypes(node, NodeType.Scalar, ScalarType.Number, NumberType.Int64, MapType.NotApplicable);
+		}
+
+		private static void AssertNodeIsDefinedScalarNumber(Node node) {
+			Assert.That(node.IsDefined);
+			Assert.That(node.IsScalar);
+			Assert.That(node.IsNumber);
+		}
+
+		private void AssertNodeHasTypes(Node node, NodeType nodeType, ScalarType scalarType, NumberType numberType, MapType mapType) {
+			Assert.AreEqual(node.NodeType, nodeType);
+			Assert.AreEqual(node.ScalarType, scalarType);
+			Assert.AreEqual(node.NumberType, numberType);
+			Assert.AreEqual(node.MapType, mapType);
+		}
+	}
+}
