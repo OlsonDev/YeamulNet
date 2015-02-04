ff5cf7297d7a06b6266ad45ef1e787647cabe01f
Josh Olson <Olson.dev@Gmail.com>
2015-02-04 14:49:23 -0600

    Add unit tests for implicit unsigned construction

--- Yeamul.Test/Test_Node_Implicit_Ctor.cs
+++ Yeamul.Test/Test_Node_Implicit_Ctor.cs
@@ -25,6 +25,27 @@ namespace Yeamul {
 			AssertNodeHasTypes(node, NodeType.Scalar, ScalarType.Number, NumberType.Int64, MapType.NotApplicable);
 		}
 
+		[Test]
+		public void CanConstructImplicitlyFromUInt16() {
+			Node node = UInt16.MaxValue;
+			AssertNodeIsDefinedScalarNumber(node);
+			AssertNodeHasTypes(node, NodeType.Scalar, ScalarType.Number, NumberType.UInt16, MapType.NotApplicable);
+		}
+
+		[Test]
+		public void CanConstructImplicitlyFromUInt32() {
+			Node node = UInt32.MaxValue;
+			AssertNodeIsDefinedScalarNumber(node);
+			AssertNodeHasTypes(node, NodeType.Scalar, ScalarType.Number, NumberType.UInt32, MapType.NotApplicable);
+		}
+
+		[Test]
+		public void CanConstructImplicitlyFromUInt64() {
+			Node node = UInt64.MaxValue;
+			AssertNodeIsDefinedScalarNumber(node);
+			AssertNodeHasTypes(node, NodeType.Scalar, ScalarType.Number, NumberType.UInt64, MapType.NotApplicable);
+		}
+
 		private static void AssertNodeIsDefinedScalarNumber(Node node) {
 			Assert.That(node.IsDefined);
 			Assert.That(node.IsScalar);
